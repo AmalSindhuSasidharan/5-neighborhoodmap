5d14c2d812d49d43e08390128cec01ddc8f57116
cuser <amal0109@gmail.com>
2017-10-23 02:00:17 +0530

    added markers

--- pro 5 neighborhood map/js/app.js
+++ pro 5 neighborhood map/js/app.js
@@ -1,5 +1,42 @@
+var locations=[
+                {
+                    title:"Hill Palace Museum",
+                    location:{lat:9.9526386,lng:76.3639139}
+                 },
+                 {
+                    title:"Hindutemple Sree Poornathrayeesa temple",
+                    location:{lat:9.9450109,lng:76.342111}
+                 },
+                 {
+                    title:"RLV College of Fine Arts",
+                    location:{lat:9.9422187,lng:76.3449798}
+                 },
+                 {
+                    title:"Vegetarisches Sree Saravana Bhavan",
+                    location:{lat:9.9533481,lng:76.3408252}
+                 },
+                 {
+                    title:"Vijaya Kumara Menon Hospital",
+                    location:{lat:9.9526027,lng:76.3407021}
+                 },
+                 {
+                    title:"Vyttila Mobility Hub",
+                    location:{lat:9.9686167,lng:76.3214095}
+                 },
+                 {
+                    title:"State Bank Of India",
+                    location:{lat:9.9518318,lng:76.3404544}
+                 },
+                 {
+                    title:"LuLu Mall",
+                    location:{lat:10.0270753,lng:76.3080901}
+                 }
+                 ];
 
-function initMap(){
+
+var markers = [];
+
+var initMap=function(){
 
  map=new google.maps.Map(document.getElementById('mapview'),{
         center:{
@@ -8,6 +45,21 @@ function initMap(){
         },
         zoom:12
     });
+
+ for(var i=0;i<locations.length;i++){
+
+   var position = locations[i].location;
+   var title = locations[i].title;
+
+   var marker = new google.maps.Marker({
+     map: map,
+     position: position,
+     title: title,
+     animation: google.maps.Animation.DROP,
+     id: i
+          });
+   // markers.push(marker);
+ }
 
 
 
